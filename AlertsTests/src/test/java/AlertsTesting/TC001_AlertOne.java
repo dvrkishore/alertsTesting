21be5710d4b3dc4020632ed94f2204d0b9d5339c
dvrkishore <mesdontha@gmail.com>
2020-12-30 16:56:08 +0000

    My First Commit

---
+++ AlertsTests/src/test/java/AlertsTesting/TC001_AlertOne.java
@@ -0,0 +1,48 @@
+package AlertsTesting;
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC001_AlertOne {
+	
+	public static WebDriver dr = null;
+	
+	@Test
+	public static void BrowserLaunch_ApplicationLaunch() throws InterruptedException
+	{
+		System.out.println("Initializing the browser");
+		System.out.println("Launching Chrome Browser");
+		System.setProperty("webdriver.chrome.driver",".//src//main//resources//Drivers//chromedriver.exe");
+		dr = new ChromeDriver();
+		dr.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		dr.manage().window().maximize();
+		System.out.println("Chrome Browser launched successfully and maximized successfully");
+		
+		System.out.println("Selenium Script for testing first type of Alert");
+		dr.get("https://www.w3schools.com/js/tryit.asp?filename=tryjs_alert");
+		Thread.sleep(3000);
+		dr.findElement(By.id("accept-choices")).click();
+		Thread.sleep(2500);
+		
+		dr.switchTo().frame("iframeResult");
+		WebElement alertButton = dr.findElement(By.cssSelector("button[onclick='myFunction()']"));
+		alertButton.click();
+		Thread.sleep(3000);
+		String expectedAlertMessage = "I am an alert box!";
+		String actualAlertMessage = dr.switchTo().alert().getText();
+		Assert.assertEquals(expectedAlertMessage,actualAlertMessage);
+		dr.switchTo().alert().accept();
+		 
+		Thread.sleep(3000);
+
+		dr.quit();
+	}
+
+}
